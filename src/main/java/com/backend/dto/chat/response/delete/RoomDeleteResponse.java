870fcf1307558d28c81149da62785422a10c676d
Junad-Park <qkrwnsgk1624@icloud.com>
2023-11-30 02:31:10 +0900

    refactor: chat 관련 dto snake case 표기로 변경

--- src/main/java/com/backend/dto/chat/response/delete/RoomDeleteResponse.java
+++ src/main/java/com/backend/dto/chat/response/delete/RoomDeleteResponse.java
@@ -3,8 +3,10 @@ package com.backend.dto.chat.response.delete;
 import com.backend.dto.bases.BaseResponse;
 import com.backend.dto.bases.ResponseMessage;
 import com.backend.dto.bases.ResponseStatus;
+import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
+@Getter
 @SuperBuilder
 public class RoomDeleteResponse extends BaseResponse {
     private final Long id;
