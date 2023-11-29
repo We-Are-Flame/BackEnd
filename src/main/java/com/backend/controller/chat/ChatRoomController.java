471f04c4fbcb3928301240d96808acc80e651f19
Junad-Park <qkrwnsgk1624@icloud.com>
2023-11-30 02:18:40 +0900

    refactor: chat controller log 제거

--- src/main/java/com/backend/controller/chat/ChatRoomController.java
+++ src/main/java/com/backend/controller/chat/ChatRoomController.java
@@ -7,7 +7,6 @@ import com.backend.dto.chat.response.delete.RoomDeleteResponse;
 import com.backend.dto.chat.response.read.ChatResponseList;
 import com.backend.dto.chat.response.read.ChatUserResponseList;
 import com.backend.dto.chat.response.create.RoomCreateResponse;
-import com.backend.dto.chat.response.read.RoomResponse;
 import com.backend.dto.chat.response.read.RoomResponseList;
 import com.backend.service.chat.ChatService;
 import com.backend.service.chat.RoomService;
@@ -16,9 +15,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,12 +25,10 @@ public class ChatRoomController {
     private final ChatService chatService;
     private final RoomService roomService;
 
-
     @GetMapping("rooms")
     @CheckUserNotNull
     public ResponseEntity<RoomResponseList> getMyChatRooms(@CurrentMember User user){
         RoomResponseList myChatRooms = roomService.getMyChatRooms(user.getId());
-        log.info("SHOW ALL ChatList {}", myChatRooms);
         return ResponseEntity.ok(myChatRooms);
     }
 
@@ -42,7 +36,6 @@ public class ChatRoomController {
     @CheckUserNotNull
     public ResponseEntity<RoomCreateResponse> createRoom(@RequestBody RoomCreateRequest request, @CurrentMember User user) {
         String id = roomService.createChatRoom(request, user.getId());
-        log.info("CREATE Chat Room [{}]", id);
         RoomCreateResponse response = RoomCreateResponse.success(id);
         return ResponseEntity.ok(response);
     }
@@ -52,7 +45,6 @@ public class ChatRoomController {
     public ResponseEntity<RoomDeleteResponse> deleteChatRoom(@CurrentMember User user,
                                                              @PathVariable String roomId) {
         Long id = roomService.deleteChatRoom(user, roomId);
-        log.info("DELETE Chat Room [{}]", id);
         RoomDeleteResponse response = RoomDeleteResponse.success(id);
         return ResponseEntity.ok(response);
     }
