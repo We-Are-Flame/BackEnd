c744e6eb273c4f3ebd178f2fb4559e1f59c902b6
hyunjin <guswlsdl0121@gmail.com>
2023-12-10 18:30:16 +0900

    feat: isFull() 로직 수정

--- src/main/java/com/backend/dto/meeting/response/output/DetailInfoOutput.java
+++ src/main/java/com/backend/dto/meeting/response/output/DetailInfoOutput.java
@@ -1,12 +1,15 @@
 package com.backend.dto.meeting.response.output;
 
 import com.backend.dto.meeting.dto.InfoDTO;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @SuperBuilder
+@Slf4j
 public class DetailInfoOutput extends InfoDTO {
     private final Integer currentParticipants;
     private final String description;
@@ -18,8 +21,12 @@ public class DetailInfoOutput extends InfoDTO {
         this.description = description;
     }
 
+    @JsonIgnore
     public boolean isFull() {
-        return currentParticipants.equals(getMaxParticipants());
+        log.info("isFull = {}", currentParticipants.equals(getMaxParticipants() - 1));
+        log.info("currentParticipants = {}", currentParticipants);
+        log.info("maxParticipants = {}", getMaxParticipants() - 1);
+        return currentParticipants.equals(getMaxParticipants() - 1);
     }
 }
 
